b2aafffa22ccc44a8b1a820e27c0a46122ffc8e8
Thafsal <thafsalthafsu@gmail.com>
2023-09-08 16:02:52 +0530

    footer section edited

--- script.js
+++ script.js
@@ -4,29 +4,29 @@ const passwordField = document.getElementById("password");
 const emailErrMsg = document.querySelector(".email_err");
 const passwordErrMsg = document.querySelector(".password_err");
 
-        form.addEventListener("submit", function (event) {
-            let messages = [];
+form.addEventListener("submit", function (event) {
+  let messages = [];
 
-            if (emailField.value.trim() === "") {
-                messages.push("Email or phone number is required");
-            }
+  if (emailField.value.trim() === "") {
+    messages.push("Email or phone number is required");
+  }
 
-            if (passwordField.value.length < 6) {
-                messages.push("Password must be at least 6 characters long");
-            }
+  if (passwordField.value.length < 6) {
+    messages.push("Password must be at least 6 characters long");
+  }
 
-            if (messages.length > 0) {
-                event.preventDefault();
-                if (messages.includes("Email or phone number is required")) {
-                    emailErrMsg.innerText = "Email or phone number is required";
-                } else {
-                    emailErrMsg.innerText = "";
-                }
+  if (messages.length > 0) {
+    event.preventDefault();
+    if (messages.includes("Email or phone number is required")) {
+      emailErrMsg.innerText = "Email or phone number is required";
+    } else {
+      emailErrMsg.innerText = "";
+    }
 
-                if (messages.includes("Password must be at least 6 characters long")) {
-                    passwordErrMsg.innerText = "Password must be at least 6 characters long";
-                } else {
-                    passwordErrMsg.innerText = "";
-                }
-            }
-        });
+    if (messages.includes("Password must be at least 6 characters long")) {
+      passwordErrMsg.innerText = "Password must be at least 6 characters long";
+    } else {
+      passwordErrMsg.innerText = "";
+    }
+  }
+});
